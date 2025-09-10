a5429a90c18f74a759dc319aafb89c149427198a
sourabhgautam26 <154259893+sourabhgautam26@users.noreply.github.com>
2025-09-10 19:58:15 +0530

    creating config Reader

---
+++ src/main/java/com/bookstore/ConfigReader.java
@@ -0,0 +1,25 @@
+package com.bookstore;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+/**
+ * Hello world!
+ *
+ */
+public class ConfigReader
+{   private static Properties props = new Properties();
+
+    static {
+        try {
+            FileInputStream fis = new FileInputStream("config.properties");
+            props.load(fis);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to load config.properties", e);
+        }
+    }
+
+    public static String get(String key) {
+        return props.getProperty(key);
+    }
+}
